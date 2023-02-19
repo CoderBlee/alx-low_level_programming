cc7865b02fe61b68589fff83ead59a461a26eeb5
CoderBlee <gamuchiraiblessing7@gmail.com>
2023-02-19 13:59:00 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	int n1;
+
+	for (n1 = 0; n1 < 10; n1++)
+		printf("%d", n1);
+
+	printf("\n");
+
+	return (0);
+
+}
