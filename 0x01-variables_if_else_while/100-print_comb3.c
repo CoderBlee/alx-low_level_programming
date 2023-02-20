925edf4da835589cc406c6496bf94436669bf2fc
CoderBlee <gamuchiraiblessing7@gmail.com>
2023-02-20 14:21:18 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - Entry point
+* Return: Always 0
+*/
+int main(void)
+{
+	int dig1, dig2;
+
+	for (dig1 = 0; dig1 < 9; dig1++)
+	{
+
+		for (dig2 = dig1 + 1; dig2 < 10; dig2++)
+		{
+			putchar((dig1 % 10) + '0');
+			putchar((dig2 % 10) + '0');
+
+			if (dig1 == 8 && dig2 == 9)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
