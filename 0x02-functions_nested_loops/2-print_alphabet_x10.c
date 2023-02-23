fbbcb53f09784a5fc409056ccefe11efe8a98fde
CoderBlee <gamuchiraiblessing7@gmail.com>
2023-02-23 07:21:47 -0800

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#includ "main.h"
+/**
+* print_alphabet_x10 - prints alphabet 10 times in lowercase
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	char m;
+	int n;
+
+	n = 0;
+
+	while (n < 10)
+	{
+		m = 'a';
+		while (m <= 'z')
+		{
+			_putcha(m);
+			m++;
+		}
+		_putchar('\n');
+		n++;
+	}
+}
