d45b1ff0a3a122efc7a87a5a23efd2fb84cd0cf8
CoderBlee <gamuchiraiblessing7@gmail.com>
2023-02-23 11:56:25 -0800

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - check the code 
+* Return: Always 0.
+*/
+int main(void)
+{
+	char c;
+
+	c = 'A';
+	printf("%c: %d\n", c, _isupper(c));
+	c = 'a';
+	peintf("%c: %d\n", c, _issupper(c));
+	return (0);
+}
